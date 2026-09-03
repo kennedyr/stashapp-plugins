d4cfbcd1630688e46fb89b9e1559f36e341d0850
Eric Kennedy <ekennedy50@gmail.com>
2026-09-03 18:01:32 -0400

    Fix redeclare

--- plugins/videojs-overrides/tsdown.config.ts
+++ plugins/videojs-overrides/tsdown.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  format: ["esm", "cjs"],
+  format: ["iife"],
   entry: ["src/main.ts"],
   dts: false,
+  platform: 'browser',
+  sourcemap: true,
   copy: { from: 'public/*', flatten: true },
   deps: {
     alwaysBundle: ['@stashapp-plugins/shared']
